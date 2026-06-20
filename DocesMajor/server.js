99788c1dbde3ee25137575ffe9e95e77e3bede90
Gabriela Cardoso <105945791+XhScarlet@users.noreply.github.com>
2026-06-20 19:28:26 -0300

    ajustes para hospedagem

--- DocesMajor/server.js
+++ DocesMajor/server.js
@@ -310,6 +310,43 @@ const server = http.createServer((req, res) => {
         fs.writeFile(dataKitFile, resetData, err => { if(err) errorCount++; else successCount++; checkDone(); });
         fs.writeFile(dataCaixasFile, resetData, err => { if(err) errorCount++; else successCount++; checkDone(); });
 
+    } else if (req.method === 'GET') {
+        let filePath = req.url;
+        if (filePath === '/') {
+            filePath = '/view/receita.html';
+        }
+        
+        filePath = filePath.split('?')[0];
+        filePath = path.normalize(filePath).replace(/^(\.\.[\/\\])+/, '');
+        
+        const extname = String(path.extname(filePath)).toLowerCase();
+        let contentType = 'text/html';
+        switch (extname) {
+            case '.js': contentType = 'text/javascript'; break;
+            case '.css': contentType = 'text/css'; break;
+            case '.json': contentType = 'application/json'; break;
+            case '.png': contentType = 'image/png'; break;
+            case '.jpg': contentType = 'image/jpg'; break;
+            case '.svg': contentType = 'image/svg+xml'; break;
+            case '.ico': contentType = 'image/x-icon'; break;
+        }
+
+        const absolutePath = path.join(__dirname, filePath);
+        
+        fs.readFile(absolutePath, (err, content) => {
+            if (err) {
+                if (err.code === 'ENOENT') {
+                    res.writeHead(404);
+                    res.end('404 - Not Found');
+                } else {
+                    res.writeHead(500);
+                    res.end('500 - Server Error');
+                }
+            } else {
+                res.writeHead(200, { 'Content-Type': contentType });
+                res.end(content, 'utf-8');
+            }
+        });
     } else {
         res.writeHead(404);
         res.end();
